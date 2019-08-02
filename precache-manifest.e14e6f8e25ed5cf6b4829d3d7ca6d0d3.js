0027dee1dbcc6a4cb5fb6364f1165dabf73fb62f
RavenVol <r.volianiuk@gmail.com>
2019-08-02 13:38:12 +0300

    Updates

---
+++ precache-manifest.e14e6f8e25ed5cf6b4829d3d7ca6d0d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260afce20e1793fe31d86db10ebdb7cc",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "72a3c3fef57f29e09277",
+    "url": "/react_pagination/static/css/main.2a90b02e.chunk.css"
+  },
+  {
+    "revision": "9fdfa2fb17ce02f8f9d7",
+    "url": "/react_pagination/static/js/2.aae1df9f.chunk.js"
+  },
+  {
+    "revision": "72a3c3fef57f29e09277",
+    "url": "/react_pagination/static/js/main.86d3094c.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
